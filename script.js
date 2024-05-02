bd4eaf420a62ed958cb92a74c30a20492265ecb7
SAGAR PARISE <141607123+sagarparise@users.noreply.github.com>
2024-05-02 11:02:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 // your code here
+document.getElementById('urlForm').addEventListener('submit', function(event) {
+  event.preventDefault(); // prevent form submission
+  
+  // Get values from form inputs
+  var name = document.getElementById('name').value;
+  var year = document.getElementById('year').value;
+
+  // Construct URL with query string
+	var url= 'https://localhost:8080/'
+if(name){
+	url+=`?name=${name}`	
+}
+if(year){
+		if(!name){
+			url+=`?year=${year}`
+		}
+	else{
+		url+=`&year=${year}`
+	}
+	}
+	
+  
+  
+  // Update the h3 element with the generated URL
+  document.getElementById('url').textContent = url;
+});
